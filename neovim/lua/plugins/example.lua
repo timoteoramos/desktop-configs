fab315ca11ef364e02f7be35348e6d2696d11efc
Timóteo Ramos <timoteoramos@ymail.com>
2023-10-19 18:11:19 -0300

    catppuccin default flavour on neovim

--- neovim/lua/plugins/example.lua
+++ neovim/lua/plugins/example.lua
@@ -15,9 +15,7 @@ return {
     priority = 1000,
     lazy = false,
     config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-      })
+      require("catppuccin").setup()
 
       -- setup must be called before loading
       vim.cmd.colorscheme "catppuccin"
